6a6b907e79fabfa196b47f0d98a28aecddd2feee
dawid-kaminski <dawidk65@gmail.com>
2020-09-03 22:56:25 +0200

    Made styling changes to the header.css file and added a new bookPage js and css files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './navbar/Navbar.js';
 import Article from './article/Article.js';
 import BookList from './bookList/BookList.js';
 import Footer from './footer/Footer.js';
+import BookPage from './bookPage/BookPage.js';
 
 function App() {
   return (
@@ -12,8 +13,7 @@ function App() {
       <Header></Header>
       <Navbar></Navbar>
       <Article></Article>
-      <Footer></Footer>
-      <BookList></BookList>
+      <BookPage></BookPage>
     </div>
   );
 }
---
+++ src/bookPage/BookPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BookPage.css';
+
+function BookPage() {
+  return (
+    <div className="book-page">
+
+    </div>
+  );
+}
+
+export default BookPage;
