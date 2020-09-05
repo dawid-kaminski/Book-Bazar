67004bfc4fe15ab0019a6689ea779ae78520c276
dawid-kaminski <dawidk65@gmail.com>
2020-09-05 13:57:42 +0200

    Made styling changes in the header.css file, added more content to the bookpage.js file

--- src/bookPage/BookPage.js
+++ src/bookPage/BookPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './BookPage.css';
+import Header from '../header/Header.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 function BookPage() {
   return (
     <div className="book-page">
-      <div>d</div>
+      <Header></Header>
+      <div className="book-page-layout">
+        <div className="book-page-picture-info">
+          <div className="book-page-picture">
+            <div className="book-page-picture-return-button">
+              <button type="button">
+                <FontAwesomeIcon icon={faArrowLeft} color="#77798C" />
+                Return
+              </button>
+            </div>
+            <div className="book-page-picture-styling">
+
+            </div>
+          </div>
+          <div className="book-page-info">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
