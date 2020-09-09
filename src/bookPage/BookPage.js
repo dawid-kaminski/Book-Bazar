e6d184c42637d73e30fb2997212a4ec3e8b65863
dawid-kaminski <dawidk65@gmail.com>
2020-09-09 17:41:05 +0200

    Added more content and styling to the BookPage files

--- src/bookPage/BookPage.js
+++ src/bookPage/BookPage.js
@@ -13,12 +13,14 @@ function BookPage() {
           <div className="book-page-picture">
             <div className="book-page-picture-return-button">
               <button type="button">
-                <FontAwesomeIcon icon={faArrowLeft} color="#77798C" />
-                Return
+                <div className="button-arrow">
+                  <FontAwesomeIcon icon={faArrowLeft} color="#77798C" />
+                </div>
+                Back
               </button>
             </div>
             <div className="book-page-picture-styling">
-
+              <img src="https://siedmiorog.pl/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/r/proces_prz_1_.jpg" height="450px" width="450px" />
             </div>
           </div>
           <div className="book-page-info">
