642d6ff906e2dc2ab9e02a8ac3f80ae62e616fa7
dawid-kaminski <dawidk65@gmail.com>
2020-09-14 20:48:30 +0200

    Added more content and styling to the bookPage files and improved the header styling

--- src/bookPage/BookPage.js
+++ src/bookPage/BookPage.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BookPage.css';
 import Header from '../header/Header.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faFeatherAlt } from '@fortawesome/free-solid-svg-icons'
+
+// function useState(defaultValue) {
+//   const value = defaultValue
+//   const set=(setValue)=>{
+//     value = setValue
+//   }
+//   return [value, set]
+// }
 
 function BookPage() {
+
+  const [count, setCount] = useState(1)
+// const count = useState(1)[0]
+// const setCount = useState(1)[1]
+
+  var onClickButtonLeft=()=>{
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  var onClickButtonRight=()=>{
+    setCount(count + 1)
+  }
+
   return (
     <div className="book-page">
       <Header></Header>
@@ -26,6 +49,36 @@ function BookPage() {
           <div className="book-page-info">
             <div className="book-page-info-title">
               Proces
+            </div>
+            <div className="book-page-info-author">
+              <div className="feather-icon">
+                <FontAwesomeIcon icon={faFeatherAlt} color="##0D1136" />
+              </div>
+              Franz Kafka
+            </div>
+            <div className="book-page-info-description">
+              Głównym bohaterem powieści jest Józef K., kawaler,
+              prokurent bankowy, mieszkający w nieznanym z nazwy mieście,
+              określanym jako „stolica”. Pewnego dnia budzi się w swoim
+              mieszkaniu i zostaje zaskoczony najściem urzędników, którzy
+              oświadczają mu, że zostaje aresztowany, mimo iż nic złego
+              nie popełnił. Mimo aresztowania może prowadzić normalne życie,
+              musi jedynie pozostać do dyspozycji sądu. Pomimo pozorów
+              normalności w życiu K. następuje seria absurdalnych,
+              niezrozumiałych wydarzeń.
+              <a href="#">Read More</a>
+            </div>
+            <div className="book-page-info-details">
+              <div className="book-page-info-category">
+                __typename
+              </div>
+              <div className="book-page-info-category-reply">
+                Meta
+              </div>
+            </div>
+            <div className="book-page-info-button">
+              <button className="button-left" onClick={onClickButtonLeft}>-</button>{count}
+              <button className="button-right" onClick={onClickButtonRight}>+</button>
             </div>
           </div>
         </div>
