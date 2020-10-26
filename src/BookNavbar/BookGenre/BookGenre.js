76aacb71dfb192a52d566bce51c14a0e9c3bf957
dawid-kaminski <dawidk65@gmail.com>
2020-10-26 21:53:19 +0100

    added an onclick to the BookGenre component and a cursor pointer

--- src/BookNavbar/BookGenre/BookGenre.js
+++ src/BookNavbar/BookGenre/BookGenre.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './BookGenre.css';
 
+var onClickBook=()=>{
+  console.log("props")
+}
+
 function BookGenre(props) {
   return (
-    <div className="book-genre">
+    <div className="book-genre" onClick={onClickBook}>
       {props.name}
     </div>
   );
