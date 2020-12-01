4121bda6061d48d039c6bad9478d6903057fbb8a
dawid-kaminski <dawidk65@gmail.com>
2020-12-01 18:24:15 +0100

    Added the search bar to the navbar component.

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
   return (
@@ -10,8 +12,19 @@ function Navbar() {
       <div className="navbar-banner-description">
         Get your favorite books from our wide range of books.
       </div>
-      <div className="navbar-search-bar">
-        <input type="search" name="search" placeholder="Search your products from here" />
+      <div className="navbar-search-bar-container">
+        <div className="navbar-search-bar">
+          <div className="navbar-search-bar-book">
+            book
+          </div>
+          <input type="search" name="search" placeholder="Search your products from here" />
+          <div className="navbar-search-bar-search">
+            <div className="navbar-search-bar-search-icon">
+              <FontAwesomeIcon icon={faSearch} color="white"/>
+            </div>
+            Search
+          </div>
+        </div>
       </div>
     </div>
   );
