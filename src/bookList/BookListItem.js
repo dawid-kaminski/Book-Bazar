fe7ba7274273a457c6c4e3e26942994f43ea890b
dawid-kaminski <dawidk65@gmail.com>
2020-12-07 20:14:45 +0100

    Changed the styling of the book list item

--- src/bookList/BookListItem.js
+++ src/bookList/BookListItem.js
@@ -19,7 +19,9 @@ console.log(props.title)
   return (
       <div className="book-list-item" onClick={onClickBook}>
         <img src={props.img} height="180px" width="140px" />
-        {props.title}
+        <div className="book-title">
+          {props.title}
+        </div>
       </div>
     );
 }
