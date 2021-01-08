8c82481e0378942d85c3267ac719b690bfbf35a2
dawid-kaminski <dawidk65@gmail.com>
2021-01-08 18:46:39 +0100

    Changed a divs name in accordance with BEM methodology

--- src/bookPage/BookPage.js
+++ src/bookPage/BookPage.js
@@ -128,7 +128,7 @@ function BookPage() {
             <div className="book-page__info-price">
               $25
             </div>
-            <div className="book-page-info-button">
+            <div className="book-page__info-button">
               <button className="button-sign" onClick={onClickButtonLeft}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="12px" height="2px" viewBox="0 0 12 2"><rect data-name="Rectangle 522" width="12" height="2" rx="1" fill="currentColor"></rect></svg>
               </button>{count}
