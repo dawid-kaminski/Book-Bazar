737c7e66ea56daf524990dd338f10dc3c94fd7b4
dawid-kaminski <dawidk65@gmail.com>
2021-04-18 21:36:36 +0200

    Made corrections and polished the loginpopup component

--- src/logInPopUp/LogInPopUp.js
+++ src/logInPopUp/LogInPopUp.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './LogInPopUp.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fasFacebook, faFacebookSquare, faGoogle, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import {
-  faFacebook,
-  faTwitter,
-  faInstagram,
-  faGithub,
-  faLinkedin
+  faFacebookSquare,
+  faGoogle,
 } from "@fortawesome/free-brands-svg-icons";
 
 function LogInPopUp(props) {
@@ -20,7 +17,7 @@ function LogInPopUp(props) {
   return (
     <div className="loginpopup">
       <div className="loginpopup__close" onClick={onClickJoinButton}>
-        <FontAwesomeIcon icon={faFacebook} color="#009e7f" />
+        <FontAwesomeIcon icon={faTimesCircle} color="#ffffff" size="2x"/>
       </div>
       <div className="loginpopup__form">
         <div className="loginpopup__welcome">
@@ -42,10 +39,10 @@ function LogInPopUp(props) {
           <div className="loginpopup__break-positioning">or</div>
         </div>
         <div className="loginpopup__button-facebook">
-          <button className="button-fb" type="button" name="button" >Continue with Facebook</button>
+          <button className="button-fb" type="button" name="button" ><div className="fb-icon"><FontAwesomeIcon icon={faFacebookSquare} color="#ffffff" size="lg" /></div> Continue with Facebook</button>
         </div>
         <div className="loginpopup__button-google">
-          <button className="button-google" type="button" name="button" >Continue with Google</button>
+          <button className="button-google" type="button" name="button" ><div className="google-icon"><FontAwesomeIcon icon={faGoogle} color="#ffffff" size="lg" /></div> Continue with Google</button>
         </div>
         <div className="loginpopup__sign-up">
           Don't have any account? <button className="button-sign-up-or-reset-password" type="button" name="button" >Sign Up</button>
