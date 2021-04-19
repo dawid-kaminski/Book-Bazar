ded4826b8c3fd6cbce927ada0efdc2f8f62d0a4d
dawid-kaminski <dawidk65@gmail.com>
2021-04-19 20:41:18 +0200

    Added .eslintrc.js file and made corrections to checkout js and ccs files.

--- src/yourOrder/checkout.js
+++ src/yourOrder/checkout.js
@@ -1,51 +1,61 @@
-import React from 'react';
-import './checkout.css';
-import Header from '../header/Header.js';
+import React from 'react';
+import './checkout.css';
+import Header from '../header/Header.js';
 
-function CheckOut() {
+function CheckOut() {
 
-  var y = document.scrollY
+  var y = document.scrollY
 
-// document.getElementById("root").addEventListener("scroll", ())
+// document.getElementById("root").addEventListener("scroll", ())
 
-return (
-  <div className="checkout">
-    <div className="checkout-positioning">
-      <div className="checkout-details">
-        <div className="checkout-delivery-address-positioning">
-          <div className="checkout-delivery-address">
-          
-          </div>
-        </div>
-      </div>
-      <div className="checkout-basket-positioning">
-        <div className="checkout-basket">
-          <div className="checkout-basket-your-order">
-            Your Order
-          </div>
-          <div className="checkout-basket-products">
-
-          </div>
-          <div className="checkout-basket-price">
-            <div className="checkout-basket-price-sub-total">
-              Sub Total
-            </div>
-            <div className="checkout-basket-price-delivery-fee">
-              Delivery Fee
-            </div>
-            <div className="checkout-basket-price-discount">
-              Discount
-            </div>
-            <div className="checkout-basket-price-total">
-              Total (Incl. VAT)
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+return (
+	<div className="checkout">
+		<div className="checkout-positioning">
+			<div className="checkout-details">
+				<div className="checkout-delivery-address-positioning">
+					<div className="checkout-delivery-address">
+						sad
+					</div>
+				</div>
+				<div className="checkout-delivery-address-positioning">
+					<div className="checkout-delivery-address">
+					</div>
+				</div>
+					<div className="checkout-delivery-address-positioning">
+						<div className="checkout-delivery-address">
+						</div>
+					</div>
+				<div className="checkout-delivery-address-positioning">
+					<div className="checkout-delivery-address">
+					</div>
+				</div>
+			</div>
+			<div className="checkout-basket-positioning">
+				<div className="checkout-basket">
+					<div className="checkout-basket-your-order">
+						Your Order
+					</div>
+					<div className="checkout-basket-products">
+					</div>
+					<div className="checkout-basket-price">
+						<div className="checkout-basket-price-sub-total">
+							Sub Total
+						</div>
+						<div className="checkout-basket-price-delivery-fee">
+							Delivery Fee
+						</div>
+						<div className="checkout-basket-price-discount">
+							Discount
+						</div>
+						<div className="checkout-basket-price-total">
+							Total (Incl. VAT)
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 )
-
 }
 
-export default CheckOut;
+export default CheckOut;
