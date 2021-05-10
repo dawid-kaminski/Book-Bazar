4fe90cbedc9927697787f568f5051de70d8df628
dawid-kaminski <dawidk65@gmail.com>
2021-05-10 18:29:38 +0200

    Improved styling and added more content

--- src/basket/Basket.js
+++ src/basket/Basket.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import './Basket.css';
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBag, faTimesCircle, faPlus, faMinus, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function Basket() {
 
@@ -26,7 +28,15 @@ function Basket() {
 		return(
 			<div className="closed-basket">
 				<button className="basket" type="button" name="button" onClick={onClickOpenBasketButton}>
-					<div className="books-amount">{cartAmountSum}</div>
+					<div className="closed-basket-items-amount">
+						<FontAwesomeIcon icon={faShoppingBag} color="#ffffff"/>
+						<div className="books-amount-closed-basket">
+							{cartAmountSum} Items
+						</div>
+					</div>
+					<div className="closed-basket-price">
+						$
+					</div>
 				</button>
 			</div>
 		)
@@ -35,9 +45,63 @@ function Basket() {
 	const OpenBasket = ()=>{
 		return(
 			<div className="open-basket" >
-				<button className="basket" type="button" name="button" onClick={onClickCloseBasketButton}>
-					<div className="books-amount">{cartAmountSum}</div>
-				</button>
+				<div className="open-basket-items-amount">
+					<FontAwesomeIcon icon={faShoppingBag} color="#rgb(0, 158, 127)" size="2x"/>
+					<div className="books-amount-open-basket">{cartAmountSum} Item</div>
+					<button className="close-basket" onClick={onClickCloseBasketButton}>
+						<FontAwesomeIcon icon={faTimesCircle} color="#rgb(0, 158, 127)" size="2x"/>
+					</button>
+				</div>
+				<div className="open-basket-books-list">
+					<div className="open-basket-item">
+						<div className="open-basket-book-counter">
+							<button className="plus-counter">
+								<FontAwesomeIcon icon={faPlus} color="#rgb(119, 121, 140)"/>
+							</button>
+							<div className="book-count-state">
+
+							</div>
+							<button className="minus-counter">
+								<FontAwesomeIcon icon={faMinus} color="#rgb(119, 121, 140)"/>
+							</button>
+						</div>
+						<div className="book-picture">
+
+						</div>
+						<div className="book-details">
+							<div className="book-name">
+
+							</div>
+							<div className="book-piece-price">
+								$60
+							</div>
+							<div className="">
+
+							</div>
+						</div>
+						<div className="book-total-price">
+							$60.00
+						</div>
+						<div className="book-cancelation">
+							<button className="close-basket" onClick={onClickCloseBasketButton}>
+								<FontAwesomeIcon icon={faTimes} size="1x"/>
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="open-basket-voucher">
+					<button className="voucher">
+						Do you have a voucher?
+					</button>	
+				</div>
+				<div className="open-basket-checkout">
+					<div className="open-basket-checkout-typing">
+						Checkout
+					</div>
+					<div className="open-basket-checkout-price">
+						$60
+					</div>
+				</div>
 			</div>
 		)
 	}
